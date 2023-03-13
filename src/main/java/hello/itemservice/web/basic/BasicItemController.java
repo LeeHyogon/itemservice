6b27cb3beb5d713ac07dd188c2a04fcecd187fc5
이효곤 <LeeHyogon>
2023-03-13 21:01:45 +0900

    Item edit apply

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -49,6 +49,7 @@ public class BasicItemController {
         //model.addAttribute("item", item); //자동 추가,생략 가능
         return "basic/item";
     }
+    
     @PostMapping("/add")
     public String saveV3(@ModelAttribute Item item){
         //모델이름 Item->item으로 자동변환.
@@ -61,6 +62,19 @@ public class BasicItemController {
         itemRepository.save(item);
         return "basic/item";
     }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "/basic/editForm";
+    }
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId,@ModelAttribute Item item) {
+        itemRepository.update(itemId,item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
 
     /**
      * 테스트용 데이터 추가
