1a82b7e6cd64fd774682a8cfe405b8cce9052ae4
이효곤 <LeeHyogon>
2023-03-13 21:23:52 +0900

    Complete practiceSpringmvc Project

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -62,12 +63,20 @@ public class BasicItemController {
         itemRepository.save(item);
         return "basic/item";
     }
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String saveV5(@ModelAttribute Item item){
         //모델이름 Item->item으로 자동변환.
         itemRepository.save(item);
         //model.addAttribute("item", item); //자동 추가,생략 가능
         return "redirect:/basic/items/"+item.getId();
+    }
+    @PostMapping("/add")
+    public String saveV6(@ModelAttribute Item item, RedirectAttributes redirectAttributes){
+        //모델이름 Item->item으로 자동변환.
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
